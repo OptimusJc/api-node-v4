84d099c41ef3e0fbdcbce05a23a54cc9e149cf34
OptimusJc <optimus.prime.jc@gmail.com>
2022-11-29 21:42:22 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* 
+    BASICS OF NODE
+*/
+const http = require("http");
+
+// * request, response
+const server = http.createServer((req, res) => {
+	if (req.method === "GET" && req.url === "/") {
+		res.statusCode = 200;
+		res.statusMessage;
+		console.log("hello from server");
+		res.end();
+	}
+});
+
+server.listen(3000, console.log("Server on http://localhost:3000/"));
